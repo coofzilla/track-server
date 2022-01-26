ddaadd8f472402ce18446255d5613f43bab835fe
coofzilla <jeric614@yahoo.com>
2022-01-26 22:05:31 +0900

    established test connection f/mongo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-const express = require("express");
-
+import express from "express";
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+//rules for import ECMAScript modules
+//extension .js
+//package-json   "type": "module"
+import { mongoURI } from "../config/dev.js";
 const app = express();
+
+mongoose.connect(mongoURI);
+
+const listingSchema = new Schema(
+  {
+    name: String,
+    summary: String,
+  },
+  //use preexisting collection
+  { collection: "listingsAndReviews" }
+);
+
+const Listing = mongoose.model("listingsAndReview", listingSchema);
+
+const findListing = async () => {
+  const response = await Listing.find({});
+  console.log(response);
+};
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -8,4 +31,5 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
   console.log("LISTENING ON 3000");
+  findListing();
 });
