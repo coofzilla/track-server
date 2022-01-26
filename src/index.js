a257f1dfb09a4625d1dd5a266f28adbca51c4f65
coofzilla <jeric614@yahoo.com>
2022-01-26 22:41:46 +0900

    query mongo w/id

--- src/index.js
+++ src/index.js
@@ -11,17 +11,19 @@ mongoose.connect(mongoURI);
 
 const listingSchema = new Schema(
   {
+    _id: String,
     name: String,
     summary: String,
   },
   //use preexisting collection
+  //collection name goes here
   { collection: "listingsAndReviews" }
 );
 
-const Listing = mongoose.model("listingsAndReview", listingSchema);
+const Listing = mongoose.model("listingsAndReviews", listingSchema);
 
 const findListing = async () => {
-  const response = await Listing.find({});
+  const response = await Listing.findById(10006546);
   console.log(response);
 };
 
