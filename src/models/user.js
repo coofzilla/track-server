32c97330b90025786674d93920f51dcc5a9f243f
coofzilla <jeric614@yahoo.com>
2022-01-27 13:47:46 +0900

    pw salted

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
   email: {
@@ -12,6 +13,40 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+//can't use arrow because context of this refers to global
+userSchema.pre("save", function () {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      return next(err);
+    });
+    user.password = hash;
+    next();
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+  const user = this;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+      if (!isMatch) {
+        return reject(false);
+      }
+
+      resolve(true);
+    });
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
