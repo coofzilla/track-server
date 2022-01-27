48edfa90261871539c8f7a24aaf79297c0e1dddd
coofzilla <jeric614@yahoo.com>
2022-01-27 11:16:49 +0900

    create JWT token

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,18 @@
 import express from "express";
 import User from "../models/user.js";
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.create({ email, password });
-
-  res.send("made post request");
+  try {
+    const user = await User.create({ email, password });
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
 });
 
 export default router;
